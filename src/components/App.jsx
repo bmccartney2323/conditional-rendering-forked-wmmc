130ca838943583d48ec9c397bcc6e5ca48aed90b
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-11-05 13:50:17 -0500

    Finished lesson!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Login from "./Login.jsx"
+
+var isLoggedIn = false;
+
+const currentTime = new Date().getHours();
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{
+ // isLoggedIn ? <h1>Hello</h1> : <Login />
+// Special case of the AND operator with left to right processing
+  currentTime>12 && <h1>Why are you still working?</h1>
+// As above but with ternary operator
+//  currentTime>12 ? <h1>Why are you still working?</h1> : null
+}</div>;
 }
 
 export default App;
